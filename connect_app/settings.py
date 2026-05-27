9812d3db1d159f2c52d4365c748a1e4d8799f1ec
t.nakamura <t.nakamura.jwoa@gmail.com>
2026-05-27 16:08:49 +0900

    '本番設定'

---
+++ connect_app/settings.py
@@ -0,0 +1,77 @@
+from .settings_common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_connect',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+    },
+
+    "rds": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "bonus_db",
+        "USER": "admin",
+        "PASSWORD": "#1qaz3edc5tgb#",
+        "HOST": "jwoashop-prod-mysql.cvposccznprz.ap-northeast-1.rds.amazonaws.com",
+        "PORT": "3306",
+        "OPTIONS": {
+            "charset": "utf8mb4"
+        },
+    },
+}
+
+# ロギング設定
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    # ロガー設定
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+
+        'connect': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラ設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタ設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ pymysql.install_as_MySQLdb()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connect_app.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connect_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
